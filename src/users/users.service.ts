259fede18d59b33ac538b3b276ae9e864a970dfa
Heytor Cavalcanti <heytor01@live.com>
2022-06-15 07:38:10 -0300

    feat: initial structure from project and implemented users routes

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.userModel.find({}).limit(100).lean();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById({ _id: id }).lean();
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel
+      .findOneAndUpdate({ _id: id }, { $set: updateUserDto }, { new: true })
+      .exec();
+  }
+
+  remove(id: string) {
+    return this.userModel.deleteOne({ _id: id }).exec();
+  }
+}
