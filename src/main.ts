bf8d0aff25b252e15a83ccf21ad14454501b4bbd
Heytor Cavalcanti <heytor01@live.com>
2022-07-08 13:03:17 -0300

    fix: remove cors configs

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['*'],
-    methods: ['*'],
-    credentials: true,
-  });
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT);
 }
